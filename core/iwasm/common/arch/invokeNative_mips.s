beb9224ef95c9b23d510d988b9ccb09a48fe903f
sizumita <github@smdr.io>
2024-12-08 15:42:52 +0900

    fix mips native

--- core/iwasm/common/arch/invokeNative_mips.s
+++ core/iwasm/common/arch/invokeNative_mips.s
@@ -22,7 +22,7 @@ invokeNative:
     .fmask 0x00000000, 0
 
     /* Fixed part of frame */
-    subu $sp, 8
+    addiu $sp, $sp, -8
 
     /* save registers */
     sw $31, 4($sp)
@@ -32,34 +32,42 @@ invokeNative:
     move $fp, $sp
 
     /* allocate enough stack space */
-    sll $11, $6, 2  /* $11 == arg_num * 4 */
-    subu $sp, $11
+    sll $11, $6, 2  /* $11 = arg_num * 4 */
+    addiu $sp, $sp, 0  # ダミー行 (必要なら削除)
+    addiu $sp, $sp, 0  # ダミー行 (必要なら削除)
+    # 上記ダミーはシンタックス的には不要だが説明用
 
-    /* make 8-byte aligned */
-    and $sp, ~7
+    # arg_num * 4分だけspを下げる
+    subu $sp, $sp, $11
+
+    # make 8-byte aligned: マスク値をレジスタにロードしてANDする
+    li $t0, -8
+    and $sp, $sp, $t0
 
     move $9, $sp
     move $25, $4    /* $25 = func_ptr */
 
 push_args:
-    beq $6, 0, done /* arg_num == 0 ? */
+    beq $6, $zero, done /* if arg_num == 0, jump done */
     lw $8, 0($5)    /* $8 = *args */
-    sw $8, 0($9)    /* store $8 to stack */
-    addu $5, 4      /* args++ */
-    addu $9, 4      /* sp++ */
-    subu $6, 1      /* arg_num-- */
+    sw $8, 0($9)     /* store $8 to stack */
+    # args++, sp++ (4byte increment)
+    addiu $5, $5, 4
+    addiu $9, $9, 4
+    addiu $6, $6, -1
     j push_args
 
 done:
-    lw $4, 0($sp)   /* Load $4..$7 from stack */
+    # 以下は引数をロードする処理例
+    # 本来は整数と浮動小数点で領域を分けるべきだが、ここでは形式修正のみ
+    lw $4, 0($sp)
     lw $5, 4($sp)
     lw $6, 8($sp)
     lw $7, 12($sp)
-    ldc1 $f12, 0($sp) /* Load $f12, $f13, $f14, $f15 */
+    ldc1 $f12, 0($sp)
     ldc1 $f14, 8($sp)
 
-    jalr $25       /* call function */
-
+    jalr $25
     nop
 
     /* restore saved registers */
@@ -68,7 +76,7 @@ done:
     lw $fp, 0($sp)
 
     /* pop frame */
-    addu $sp, $sp, 8
+    addiu $sp, $sp, 8
 
     j $31
     .end invokeNative
